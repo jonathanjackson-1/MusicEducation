abe7aa54157972e2fbf337aab83b01892b5f0cda
Jonathan Jackson <Jonathanjackson.work1@gmail.com>
2025-10-16 14:51:41 -0400

    Fix jest-expo configuration

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig } = require('@expo/jest-expo/legacy');
+const { getDefaultConfig } = require('jest-expo/legacy');
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = {
   ...config,
-  preset: '@expo/jest-expo/ios',
+  preset: 'jest-expo/ios',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|expo(nent)?|@expo|expo-status-bar)/)'
